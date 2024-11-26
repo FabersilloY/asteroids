23305ee3cb6973a90b07d2e85428b4d8440c53a5
FabersilloY <fabersilloy@gmail.com>
2024-11-26 17:24:42 -0500

    Override

--- main.py
+++ main.py
@@ -11,12 +11,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
+
         screen.fill((0,0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+        dt = clock.tick(60) / 1000
+        print(dt)
         pygame.display.flip()
+        
 if __name__ == "__main__":
     main()
